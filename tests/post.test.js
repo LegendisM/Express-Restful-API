e99537167ab700da36c775b4ac64bc98b77e7ef3
LegendisM <makenkap@gmail.com>
2022-07-12 10:54:13 +0430

    add supertest

---
+++ tests/post.test.js
@@ -0,0 +1,5 @@
+describe("GET /post", () => {
+    it("get response with json", () => {
+        expect(4).toBe(4);
+    });
+});
---
+++ tests/user.test.js
@@ -0,0 +1,7 @@
+const app = require("../app");
+
+describe("GET /user", () => {
+    it("response with json", () => {
+        expect(1).toBe(1);
+    });
+});
