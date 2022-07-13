62de3408bc0d51beb2f866c9507171c21473ee36
LegendisM <makenkap@gmail.com>
2022-07-13 12:20:45 +0430

    Update user.test.js

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,29 +1,34 @@
 const app = require('../app');
 const request = require('supertest')(app);
 
+const exampleData = {
+    username: 'test',
+    email: 'test@example.com',
+    password: 'test1234'
+}
+
 describe("User Service", () => {
 
-    describe("Login / Register", () => {
+    describe("Login / Register", async () => {
 
-        it("Login Successfully", async () => {
-
-            let response = await request.post('/api/user/login')
-                .set("token", process.env.TOKEN_SECRET)
-                .send({ username: "Test", password: "abcd1234" });
-
-            expect(response.statusCode).toBe(200);
-            expect(response.body).toBe({ state: true });
-
-        });
-
-        it("Register Successfully", async () => {
+        await it("Register Successfully", async () => {
 
             let response = await request.post('/api/user/register')
                 .set('token', process.env.TOKEN_SECRET)
-                .send({ username: 'LegendisM', email: 'makenkap@gmail.com', password: 'abcd1234' });
+                .send({ username: exampleData.username, email: exampleData.email, password: exampleData.password });
 
             expect(response.statusCode).toBe(201);
-            expect(response.body).toBe({ state: true });
+
+        });
+
+        await it("Login Successfully", async () => {
+
+            let response = await request.post('/api/user/login')
+                .set("token", process.env.TOKEN_SECRET)
+                .send({ username: exampleData.username, password: exampleData.password });
+
+            expect(response.statusCode).toBe(200);
+
         });
 
     });
