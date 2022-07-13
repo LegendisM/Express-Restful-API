22e5693607bf86c7dc21010b8cbc783f1cd62466
LegendisM <makenkap@gmail.com>
2022-07-13 15:29:38 +0430

    add services

---
+++ services/post.service.js
@@ -0,0 +1,21 @@
+const PostModel = require('../models/post.model');
+
+exports.getAllPosts = async () => {
+    //! s o o n
+}
+
+exports.getPostbyId = async () => {
+    //! s o o n
+}
+
+exports.createPost = async () => {
+    //! s o o n
+}
+
+exports.updatePost = async () => {
+    //! s o o n
+}
+
+exports.deletePost = async () => {
+    //! s o o n
+}
---
+++ services/user.service.js
@@ -0,0 +1,21 @@
+const UserModel = require('../models/user.model');
+
+exports.getAllUsers = async () => {
+    //! s o o n
+}
+
+exports.getUserbyId = async () => {
+    //! s o o n
+}
+
+exports.createUser = async () => {
+    //! s o o n
+}
+
+exports.updateUser = async () => {
+    //! s o o n
+}
+
+exports.deleteUser = async () => {
+    //! s o o n
+}
